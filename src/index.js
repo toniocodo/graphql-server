949beef2f320b2ae3c17b79a0be6d0d91668e071
codognoa <antoine.codogno@kapsch.net>
2017-09-26 16:47:08 +0200

    setup db

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
+const schema = require('./schema')
+const connectMongo = require('./mongo-connector')
+
+const start = async () => {
+  const mongo = await connectMongo()
+  let app = express()
+  app.use('/graphql', bodyParser.json(), graphqlExpress({
+    context: { mongo },
+    schema
+  }))
+  app.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql',
+  }))
+
+  const PORT = 3000
+  app.listen(PORT, () => {
+    console.log(`GraphQL server running on port ${PORT}.`)
+  })
+}
+
+start()
